e722842f75a99c7783cefcf0165d49cbd8f378e2
15DEEPIKATHANGAVEL <164891723+15DEEPIKATHANGAVEL@users.noreply.github.com>
2026-06-29 00:12:45 +0530

    SL4J and spring core maven exercises

---
+++ Spring core maven/Exercise 4/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,30 @@
+package com.library.service;
+
+
+import com.library.repository.BookRepository;
+
+
+public class BookService {
+
+
+private BookRepository repository;
+
+
+public void setRepository(BookRepository repository){
+
+this.repository = repository;
+
+}
+
+
+
+public void addBook(){
+
+repository.saveBook();
+
+System.out.println("Service completed");
+
+}
+
+
+}
